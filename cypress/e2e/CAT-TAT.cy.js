f29052fc41e7047d2305cea3441ba5f8c4a92c9f
Your Name <anderson.ramosmaciel@gmail.com>
2024-10-21 15:17:41 -0300

    Finalizando os exercicios da aula 3

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -14,7 +14,7 @@ describe('Central de atendimento ao cliente TAT', () => {
     cy.get('#lastName').type('Maciel', {delay: 100})
     cy.get('#email').type('anderson.maciel@gmail.com')
     cy.get('#open-text-area').type('TESTE ANDERSON')
-    cy.get('.button').click();
+    cy.contains('.button', 'Enviar').click();
     cy.get('.success > strong').contains('Mensagem enviada com sucesso').should('be.visible')
   });
   it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
@@ -22,7 +22,7 @@ describe('Central de atendimento ao cliente TAT', () => {
     cy.get('#lastName').type('Maciel', {delay: 100})
     cy.get('#email').type('anderson.maciel@gmail.')
     cy.get('#open-text-area').type('TESTE ANDERSON')
-    cy.get('.button').click();
+    cy.contains('.button', 'Enviar').click();
     cy.get('.error > strong').contains('Valide os campos obrigatórios!').should('be.visible')
 
   });
@@ -42,20 +42,28 @@ describe('Central de atendimento ao cliente TAT', () => {
     cy.get('#lastName').type('Maciel', {delay: 100})
     cy.get('#email').type('anderson.maciel@gmail.')
     cy.get('#open-text-area').type('TESTE ANDERSON')
-    cy.get('.button').click();
+    cy.contains('.button', 'Enviar').click();
     cy.get('.error > strong').contains('Valide os campos obrigatórios!').should('be.visible')
   });
 
-  it.only('Preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+  it('Preenche e limpa os campos nome, sobrenome, email e telefone', () => {
 
-    cy.get('#firstName').type('Anderson',{delay: 200}).should('have.value', 'Anderson').clear().should('have.value', '');
-    
-    cy.get('#lastName').type('Maciel', {delay: 100}).should('have.value', 'Maciel').clear().should('have.value', '');
-    
+    cy.get('#firstName').type('Anderson',{delay: 200}).should('have.value', 'Anderson').clear().should('have.value', '');    
+    cy.get('#lastName').type('Maciel', {delay: 100}).should('have.value', 'Maciel').clear().should('have.value', '');    
     cy.get('#email').type('anderson.maciel@gmail.com').should('have.value', 'anderson.maciel@gmail.com').clear().should('have.value', '');
-    
     cy.get('#phone').type('123').should('have.value', '123').clear().should('have.value', '');
     
+  });
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+
+    cy.contains('.button', 'Enviar').click();
+    cy.get('.error').should('be.visible')
+
+  });
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit();
+    cy.get('.success').should('be.visible')
   });
 
 })

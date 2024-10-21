efb73134a5196abddde9ab8a1b03ebab49a604bb
Your Name <anderson.ramosmaciel@gmail.com>
2024-10-21 13:32:41 -0300

    exercicio extra 5

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -27,11 +27,35 @@ describe('Central de atendimento ao cliente TAT', () => {
 
   });
   it('Teste: Campo telefone deve apenas aceitar valores numericos', () => {
-    cy.get('#phone').type('texto');
-    cy.get('#phone').should('have.value', '');
+    cy.get('#phone')
+      .type('texto')
+      .should('have.value', '');
 
-    cy.get('#phone').type('123')
-    cy.get('#phone').should('have.value', '123');
+
+    cy.get('#phone')
+      .type('123')
+      .should('have.value', '123')
+  });
+
+  it('Exibe mensagem de erro quando o telefone se torna obrigatório, mas não é preenchido antes do envio do formulario', () => {
+    cy.get('#firstName').type('Anderson',{delay: 200})
+    cy.get('#lastName').type('Maciel', {delay: 100})
+    cy.get('#email').type('anderson.maciel@gmail.')
+    cy.get('#open-text-area').type('TESTE ANDERSON')
+    cy.get('.button').click();
+    cy.get('.error > strong').contains('Valide os campos obrigatórios!').should('be.visible')
+  });
+
+  it.only('Preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+
+    cy.get('#firstName').type('Anderson',{delay: 200}).should('have.value', 'Anderson').clear().should('have.value', '');
+    
+    cy.get('#lastName').type('Maciel', {delay: 100}).should('have.value', 'Maciel').clear().should('have.value', '');
+    
+    cy.get('#email').type('anderson.maciel@gmail.com').should('have.value', 'anderson.maciel@gmail.com').clear().should('have.value', '');
+    
+    cy.get('#phone').type('123').should('have.value', '123').clear().should('have.value', '');
+    
   });
 
 })
